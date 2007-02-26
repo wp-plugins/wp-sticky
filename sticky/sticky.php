475268531f85987f8c65ba581aaa6c6fd82b515d
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-02-26 18:29:22 +0000

    Minor Update
    git-svn-id: https://plugins.svn.wordpress.org/wp-sticky/trunk@8075 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sticky/sticky.php
+++ sticky/sticky.php
@@ -32,6 +32,10 @@ Author URI: http://www.lesterchan.net
 load_plugin_textdomain('wp-sticky', 'wp-content/plugins/sticky');
 
 
+### Sticky Table Name
+$wpdb->sticky = $table_prefix . 'sticky';
+
+
 ### Function: Sticky Option Menu
 add_action('admin_menu', 'sticky_menu');
 function sticky_menu() {
@@ -41,56 +45,74 @@ function sticky_menu() {
 }
 
 
-### Function: WHERE Clause Of The Sticky Query
-add_filter('posts_where', 'sticky_posts_where');
-function sticky_posts_where($where) {
+### Function: Get Sticky Option
+function get_sticky_option($option) {
 	$sticky_options = get_option('sticky_options');
-	if(intval($sticky_options['category_only']) == 0 || is_category()) {
-		$where = ' AND 0=1 '.$where;
-	}
-	return $where;
+	return $sticky_options[$option];
 }
 
 
-### Functiom: Get Sticky Posts
-add_filter('the_posts', 'sticky_the_posts');
-function sticky_the_posts($posts) {
-	global $wpdb, $wp_query, $request, $q;
-	$sticky_options = get_option('sticky_options');
-	if(!intval($sticky_options['category_only']) || is_category()) {
-		if($stickies = $wpdb->get_col("SELECT $wpdb->postmeta.Post_ID FROM $wpdb->postmeta WHERE meta_key = 'sticky' AND meta_value = '1'")) {
-			$insticky = 'NOT (ID IN ('.implode(',', $stickies).')),';
-		} else {
-			$insticky = '';
-			$stickies = array();
-		}
-		if($request == '') {
-			$request = $wp_query->request;
-		}
-		$request = str_replace(' AND 0=1 ', '', $request);
-		$wp_query->request = $request;
-		$request = preg_replace("/ORDER BY post_{$q['orderby']}/", "ORDER BY $insticky post_{$q['orderby']}", $request);
-		unset($posts);
-		$posts = array();
-		if($temp = $wpdb->get_results($request)) {
-			foreach($temp as $post) {
-				$post->is_sticky = (in_array($post->ID, $stickies));
-				array_push($posts, $post);
-			}
-		}
+### Function: Sticky Query
+if(intval(get_sticky_option('category_only')) == 1) {
+	if(true) {
+		add_filter('posts_fields', 'sticky_fields');
+		add_filter('posts_join', 'sticky_join');
+		add_filter('posts_orderby', 'sticky_orderby', 1);
 	}
-	return $posts;
+} else {
+	add_filter('posts_fields', 'sticky_fields');
+	add_filter('posts_join', 'sticky_join');
+	add_filter('posts_orderby', 'sticky_orderby', 1);
+}
+function sticky_fields($content) {
+	global $wpdb;
+	$content .= ", $wpdb->sticky.sticky_status";
+	return $content;
+}
+function sticky_join($content) {
+	global $wpdb;
+	$content .= " LEFT JOIN $wpdb->sticky ON $wpdb->sticky.sticky_post_id = $wpdb->posts.ID";
+	return $content;
+}
+function sticky_orderby($content) {
+	global $wpdb;
+	$content = "($wpdb->sticky.sticky_status = 2 AND $wpdb->sticky.sticky_status IS NOT NULL) DESC, DATE_FORMAT($wpdb->posts.post_date,'%Y-%m-%d') DESC, ($wpdb->sticky.sticky_status = 1 AND $wpdb->sticky.sticky_status IS NULL) DESC, DATE_FORMAT($wpdb->posts.post_date,'%T') DESC";
+	return $content;
+}
+
+
+### Function: Output Post Sticky Status, Sticky Or Announcement
+function post_sticky_status($before = '', $after = '', $display = true) {
+	global $id, $post;
+	$temp = '';
+	switch($post->sticky_status) {
+		case 1:
+			$temp = $before.__('Sticky', 'wp-sticky').$after;
+			break;
+		case 2:
+			$temp = $before.__('Announcement', 'wp-sticky').$after;
+			break;
+	}
+	if($display) {
+		echo $temp;
+	} else {
+		return $temp;
+	}
 }
 
 
 ### Function: Sticky The Date
 add_filter('the_date', 'sticky_the_date');
 function sticky_the_date($content) {
-	global $post, $previousday;
-	$sticky_options = get_option('sticky_options');
-	if($post->is_sticky && intval($sticky_options['display_date']) == 0) {
+	global $post, $previousday, $printed_announcement;
+	if($post->sticky_status == 2 && intval(get_sticky_option('display_date')) == 0) {
 		$previousday = '';
-		return $sticky_options['sticky_banner'];
+		if($printed_announcement) {
+			return;
+		} else {
+			$printed_announcement = true;
+			return get_sticky_option('announcement_banner');
+		}
 	}
 	return $content;
 }
@@ -100,10 +122,8 @@ function sticky_the_date($content) {
 add_filter('the_title', 'sticky_the_title');
 function sticky_the_title($content) {
 	global $post;
-	$sticky_options = get_option('sticky_options');
-	//if(preg_match('|edit.php|i', $_SERVER['REQUEST_URI']) && ($post->is_sticky)) {
-	if($post->is_sticky) {
-		 $content = $sticky_options['sticky_text'].$content;
+	if(preg_match('|edit.php|i', $_SERVER['REQUEST_URI']) && ($post->sticky_status > 0)) {
+		 $content = post_sticky_status('', '', false).': '.$content;
 	}
 	return $content;
 }
@@ -113,24 +133,41 @@ function sticky_the_title($content) {
 //add_filter('the_content', 'sticky_the_content');
 function sticky_the_content($content) {
 	global $post;
-	return ($post->is_sticky) ?
-		"<script type=\"text/javascript\">window.document.getElementById('post-{$post->ID}').parentNode.className += ' adhesive_post';</script>{$content}" :
-		$content;
+	if($post->sticky_status > 0) {
+		return "<script type=\"text/javascript\">window.document.getElementById('post-{$post->ID}').parentNode.className += ' adhesive_post';</script>$content";
+	} else {
+		return  $content;
+	}
 }
 
 
 ### Function: Processing Sticky Post
 add_action('edit_post', 'add_sticky_admin_process');
-add_action('publish_post', 'add_sticky_admin_process');
 add_action('save_post', 'add_sticky_admin_process');
 function add_sticky_admin_process($post_ID) {
-	$post_status_sticky = intval($_POST['post_status_sticky']);
-	$check_sticky = get_post_meta($post_ID, 'sticky', true);
-	if(empty($check_sticky)) {
-		add_post_meta($post_ID, 'sticky', $post_status_sticky);
+	global $wpdb;
+	$post_status_sticky_status = intval($_POST['post_status_sticky']);
+	// Normal Posts
+	if($post_status_sticky_status == 0 && intval($post_ID) > 0) {
+		$wpdb->query("DELETE FROM $wpdb->sticky WHERE sticky_post_id = $post_ID");
+	// Sticky Post/ Announcement Post
 	} else {
-		update_post_meta($post_ID, 'sticky', $post_status_sticky);
+		// Ensure No Duplicate Field
+		$check = $wpdb->get_var("SELECT sticky_status FROM $wpdb->sticky WHERE sticky_post_id = $post_ID");
+		if(!$check) {
+			$wpdb->query("INSERT INTO $wpdb->sticky VALUES($post_ID, $post_status_sticky_status)");
+		} else {
+			 $wpdb->query("UPDATE $wpdb->sticky SET sticky_status = $post_status_sticky_status WHERE sticky_post_id = $post_ID");
+		}
 	}
+}
+
+
+### Function: Delete Away Sticky If Post Is Deleted
+add_action('delete_post', 'delete_sticky_admin_process');
+function delete_sticky_admin_process($post_ID) {
+	global $wpdb;
+	$wpdb->query("DELETE FROM $wpdb->sticky WHERE sticky_post_id = $post_ID");
 }
 
 
@@ -140,12 +177,17 @@ add_action('edit_form_advanced', 'add_sticky_admin');
 add_action('simple_edit_form', 'add_sticky_admin');
 function add_sticky_admin($content) {
 	global $wpdb;
-	if(isset($_REQUEST['post'])) {
-		$post_status_sticky = intval($wpdb->get_var("SELECT meta_value FROM $wpdb->postmeta WHERE Post_ID = {$_REQUEST['post']} AND meta_key = 'sticky';"));
+	$edit_post = intval($_GET['post']);
+	$post_status_sticky_id = 0;
+	$post_status_sticky_status  = 0;
+	if($edit_post > 0) {
+		$post_status_sticky_status = intval($wpdb->get_var("SELECT sticky_status FROM $wpdb->sticky WHERE sticky_post_id = $edit_post"));
 	}
 ?>
 <div id="stickydiv">
-	<label for="post_status_sticky" class="selectit"><input type="checkbox" id="post_status_sticky" name="post_status_sticky" value="1"<?php checked($post_status_sticky, 1); ?>/>&nbsp;<?php _e('Sticky', 'wp-sticky'); ?></label>
+	<label for="post_status_announcement" class="selectit"><input type="radio" id="post_status_announcement" name="post_status_sticky" value="2"<?php checked($post_status_sticky_status, 2); ?>/>&nbsp;<?php _e('Announcement', 'wp-sticky'); ?></label>
+	<label for="post_status_sticky" class="selectit"><input type="radio" id="post_status_sticky" name="post_status_sticky" value="1"<?php checked($post_status_sticky_status, 1); ?>/>&nbsp;<?php _e('Sticky', 'wp-sticky'); ?></label>
+	<label for="post_status_normal" class="selectit"><input type="radio" id="post_status_normal" name="post_status_sticky" value="0"<?php checked($post_status_sticky_status, 0); ?>/>&nbsp;<?php _e('Normal', 'wp-sticky'); ?></label>
 </div>
 <script type="text/javascript">
 	var placement = document.getElementById("post_status_private");
@@ -170,8 +212,7 @@ function sticky_options() {
 	if($_POST['Submit']) {
 		$sticky_options['display_date'] = intval($_POST['display_date']);
 		$sticky_options['category_only'] = intval($_POST['category_only']);
-		$sticky_options['sticky_banner'] = trim($_POST['sticky_banner']);		
-		$sticky_options['sticky_text'] = $_POST['sticky_text'];
+		$sticky_options['announcement_banner'] = trim($_POST['announcement_banner']);		
 		$update_sticky_options = update_option('sticky_options', $sticky_options);
 		if($update_sticky_options) {
 			$text = '<font color="green">'.__('Sticky Options Updated', 'wp-sticky').'</font>';
@@ -194,7 +235,7 @@ function sticky_options() {
 						<option value="0"<?php selected('0', $sticky_options['category_only']); ?>><?php _e('No', 'wp-sticky'); ?></option>
 						<option value="1"<?php selected('1', $sticky_options['category_only']); ?>><?php _e('Yes', 'wp-sticky'); ?></option>
 					</select>
-					<br /><?php _e('Display sticky posts only when viewing categories.', 'wp-sticky'); ?>
+					<br /><?php _e('Display announcement and sticky posts only when viewing categories.', 'wp-sticky'); ?>
 				</td>
 			</tr>
 			<tr>
@@ -204,21 +245,14 @@ function sticky_options() {
 						<option value="0"<?php selected('0', $sticky_options['display_date']); ?>><?php _e('No', 'wp-sticky'); ?></option>
 						<option value="1"<?php selected('1', $sticky_options['display_date']); ?>><?php _e('Yes', 'wp-sticky'); ?></option>
 					</select>
-					<br /><?php _e('Displays the date instead of the <strong>Sticky Banner</strong> on sticky posts.<br />You can make use of the <strong>Sticky Text</strong> to denote a sticky post if you choose \'Yes\' for this option.', 'wp-sticky'); ?>
+					<br /><?php _e('Displays the date instead of the <strong>Announcement Banner</strong> on announcement posts.', 'wp-sticky'); ?>
 				</td>
 			</tr>
 			<tr>
-				<td valign="top"><strong><?php _e('Sticky Banner:', 'wp-sticky'); ?></strong></td>
+				<td valign="top"><strong><?php _e('Announcement Banner:', 'wp-sticky'); ?></strong></td>
 				<td>
-					<input type="text" name="sticky_banner" size="60" value="<?php echo htmlentities($sticky_options['sticky_banner']); ?>" />
+					<input type="text" name="announcement_banner" size="60" value="<?php echo htmlentities($sticky_options['announcement_banner']); ?>" />
 					<br /><?php _e('This banner is displayed instead of the date if you choose \'No\' for <strong>Display Date</strong>.', 'wp-sticky'); ?>
-				</td>
-			</tr>
-			<tr>
-				<td valign="top"><strong><?php _e('Sticky Text:', 'wp-sticky'); ?></strong></td>
-				<td>
-					<input type="text" name="sticky_text" size="60" value="<?php echo htmlentities($sticky_options['sticky_text']); ?>" />
-					<br /><?php _e('Text to display beside the post title if the post is a sticky post.', 'wp-sticky'); ?>
 				</td>
 			</tr>
 			<tr>
@@ -239,8 +273,7 @@ function sticky_init() {
 	$sticky_options = array();
 	$sticky_options['category_only'] = 0;
 	$sticky_options['display_date'] = 0;
-	$sticky_options['sticky_banner'] = __('Announcement', 'wp-sticky');
-	$sticky_options['sticky_text'] = __('Sticky: ', 'wp-sticky');
+	$sticky_options['announcement_banner'] = __('Announcement', 'wp-sticky');
 	add_option('sticky_options', $sticky_options, 'Sticky Options');
 }
 ?>
